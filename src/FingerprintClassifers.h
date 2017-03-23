a13319fe4440a44c64478a5de63f46600648e781
Lin Li <lin.li@haud.com>
2017-03-23 12:51:44 +0800

    More work

--- src/FingerprintClassifers.h
+++ src/FingerprintClassifers.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Tokenizer.h"
+#include <mutex>
 
 template <typename F> uint32_t hamming_distance(F x, F y, int displacement = 28)
 {
@@ -43,18 +44,13 @@ private:
     
     Generator m_generator;
     Permutor m_permutor;
-    
+    const static uint32_t MAX_ALLOWED_DISTANCE = 4;
 public:
     TFingerprintClassifierSimple()
     {
 	m_groupIDCounter = 0;
     }
     
-    ~TFingerprintClassifierSimple()
-    {
-
-    }
-
     virtual void Clear()
     {
 	for(int i = 0; i < Permutor::SIZE; ++i)
@@ -70,18 +66,20 @@ public:
     {
 
 	F m_permuted[Permutor::SIZE];
-	std::array<std::tuple<int, uint32_t>, Permutor::SIZE> m_groupArray;
-	
+       
 	F f = m_generator.Generate(tokens);
 	F reversed_f = m_generator.Generate_r(tokens);
 	
 	m_permutor.Permute(f, m_permuted);
 	
 	uint32_t newGroup = 0;
+	uint32_t globalMinHamming = MAX_ALLOWED_DISTANCE;
 
 	for(int i = 0; i < Permutor::SIZE; ++i)
 	{
-	    std::vector<std::pair<std::pair<F, uint32_t>, uint32_t>> fingerprints;
+	    uint32_t partitionLocalMinHamming = MAX_ALLOWED_DISTANCE;
+	    uint32_t localGroupId = 0;
+	    
 	    // Get all fingerprints whose top 28 bits are the same as the ones in f
 	    F lowerbound = m_permutor.GetLowerbound(m_permuted[i]);
 	    F upperbound = m_permutor.GetUpperbound(m_permuted[i]);
@@ -93,39 +91,21 @@ public:
 	    {
 		// Compute the Hamming distance.
 		uint32_t hammingDistance = hamming_distance<F>(m_permuted[i], lowerIter->first);
-		if(hammingDistance <= 3)
-		{	
-		    fingerprints.push_back(std::pair<std::pair<F, uint32_t>,uint32_t>(*lowerIter, hammingDistance));
+		if(hammingDistance < partitionLocalMinHamming)
+		{
+		    partitionLocalMinHamming = hammingDistance;
+		    localGroupId = lowerIter->second;	
 		}
 
 		++lowerIter;
 	    }
 
-	    if(fingerprints.empty())
+	    if(partitionLocalMinHamming < globalMinHamming)
 	    {
-		m_groupArray[i] = std::make_tuple<int, uint32_t>(0,(uint32_t)~0);
-	    }
-	    else
-	    {
-		std::sort(fingerprints.begin(), fingerprints.end(),
-			  [](const std::pair<std::pair<F, uint32_t>, uint32_t> &left,
-			     const std::pair<std::pair<F, uint32_t>, uint32_t> &right) {
-			      return left.second < right.second;
-			  });
-
-		m_groupArray[i] = std::make_tuple(fingerprints[0].first.second, 
-						  fingerprints[0].second);
-	    
+		newGroup = localGroupId;
 	    }		
 	}
 
-	std::sort(m_groupArray.begin(), m_groupArray.end(),
-		  [](const std::tuple<int, uint32_t> &left,
-		     const std::tuple<int, uint32_t> &right) {
-		      return std::get<1>(left) < std::get<1>(right);
-		  });
-
-	newGroup = std::get<0>(m_groupArray[0]);
 	if(newGroup == 0)
 	{
 	    newGroup = ++m_groupIDCounter;
@@ -150,7 +130,7 @@ public:
 		for(; i < it->second.size(); ++i)
 		{
 		    uint32_t hammingDistance = hamming_distance<F>(reversed_f, (it->second)[i].first);
-		    if(hammingDistance <= 3)
+		    if(hammingDistance <= MAX_ALLOWED_DISTANCE)
 		    {	
 			matched = true;
 			break;
@@ -175,3 +155,190 @@ public:
     
 };
 
+
+template <typename F, typename Generator, typename Permutor, typename Mutex> class TFingerprintClassifierMT : public FingerprintsClassifier
+{
+private:
+    std::atomic<uint32_t> m_groupIDCounter;
+    std::atomic<bool> m_workingFlags[Permutor::SIZE];
+    std::array<std::map<F /* The fingerprint */, uint32_t /* reference into the secondary table */>, Permutor::SIZE> m_tables;
+    std::map<uint32_t, std::vector<std::pair<F, uint32_t>>> m_secondary;
+    Mutex m_mutexForSecondary;
+    Generator m_generator;
+    Permutor m_permutor;
+    const static uint32_t MAX_ALLOWED_DISTANCE = 4;
+public:
+    TFingerprintClassifierMT()
+    {
+	m_groupIDCounter = 0;
+	for(int i = 0; i < Permutor::SIZE; ++i)
+	{
+	    m_workingFlags[i] = false;	       
+	}
+	
+    }
+    
+    virtual void Clear()
+    {
+	bool finished[Permutor::SIZE];
+	for(int i = 0; i < Permutor::SIZE; ++i)
+	{
+	    finished[i] = false;
+	}
+
+	for(int count = 0; count < Permutor::SIZE; )
+	{
+	    for(int i = 0; i < Permutor::SIZE; ++i)
+	    {
+		if(!finished[i])
+		{
+		    bool flag = m_workingFlags[i].exchange(true);
+		    if(flag)
+			continue;
+		    m_tables[i].clear();
+		    ++count;
+		    finished[i] = true;
+		    m_workingFlags[i].store(false);
+		}
+		
+	    }
+	}
+	std::lock_guard<Mutex> l(m_mutexForSecondary);
+	m_secondary.clear();
+	m_groupIDCounter = 0;
+    }
+
+    virtual uint32_t GetGroupID(const std::vector<std::u16string>& tokens)
+    {
+
+	F permuted[Permutor::SIZE];
+	
+	F f = m_generator.Generate(tokens);
+	F reversed_f = m_generator.Generate_r(tokens);
+	
+	m_permutor.Permute(f, permuted);
+	
+	uint32_t newGroup = 0;
+	uint32_t globalMinHamming = MAX_ALLOWED_DISTANCE;
+	
+	bool finished[Permutor::SIZE];
+	for(int i = 0; i < Permutor::SIZE; ++i)
+	{
+	    finished[i] = false;
+	}
+
+	for(int count = 0; count < Permutor::SIZE; )
+	{
+	    for(int i = 0; i < Permutor::SIZE; ++i)
+	    {
+		if(!finished[i])
+		{
+		    bool flag = m_workingFlags[i].exchange(true);
+		    if(flag)
+			continue;
+
+		    uint32_t partitionLocalMinHamming = MAX_ALLOWED_DISTANCE;
+		    uint32_t localGroupId = 0;
+		    // Get all fingerprints whose top 28 bits are the same as the ones in f
+		    F lowerbound = m_permutor.GetLowerbound(permuted[i]);
+		    F upperbound = m_permutor.GetUpperbound(permuted[i]);
+	
+		    auto lowerIter = m_tables[i].lower_bound(lowerbound);
+		    auto upperIter = m_tables[i].upper_bound(upperbound);
+	   	 
+		    while(lowerIter != upperIter)
+		    {
+			// Compute the Hamming distance.
+			uint32_t hammingDistance = hamming_distance<F>(permuted[i], lowerIter->first);
+			if(hammingDistance < partitionLocalMinHamming)
+			{
+			    partitionLocalMinHamming = hammingDistance;
+			    localGroupId = lowerIter->second;	
+			}
+
+			++lowerIter;
+		    }
+
+		    m_workingFlags[i].store(false);
+		
+		    if(partitionLocalMinHamming < globalMinHamming)
+		    {
+			newGroup = localGroupId;
+		    }
+		
+		    ++count;
+		    finished[i] = true;
+		}
+		
+	    }
+
+	}
+
+	if(newGroup == 0)
+	{
+	    newGroup = ++m_groupIDCounter;
+
+	    for(int i = 0; i < Permutor::SIZE; ++i)
+	    {
+		finished[i] = false;
+	    }
+	    
+	    for(int count = 0; count < Permutor::SIZE; )
+	    {
+		for(int i = 0; i < Permutor::SIZE; ++i)
+		{
+		    if(!finished[i])
+		    {
+			bool flag = m_workingFlags[i].exchange(true);
+			if(flag)
+			    continue;
+			
+			m_tables[i].insert(std::pair<F,uint32_t>(permuted[i], newGroup));
+			++count;
+			finished[i] = true;
+			m_workingFlags[i].store(false);
+		    }
+		}
+	    }
+	    std::vector<std::pair<F, uint32_t>> newMap;
+	    newMap.push_back(std::pair<F,uint32_t>(reversed_f, newGroup));
+
+	    std::lock_guard<Mutex> l(m_mutexForSecondary);
+	    m_secondary[newGroup] = newMap;
+	
+	}
+	else
+	{
+	    std::lock_guard<Mutex> l(m_mutexForSecondary);
+	    auto it = m_secondary.find(newGroup);
+	    if(it != m_secondary.end())
+	    {
+	    	bool matched = false;
+	    	int i = 0;
+	    	for(; i < it->second.size(); ++i)
+	    	{
+	    	    uint32_t hammingDistance = hamming_distance<F>(reversed_f, (it->second)[i].first);
+	    	    if(hammingDistance < MAX_ALLOWED_DISTANCE)
+	    	    {	
+	    		matched = true;
+	    		break;
+	    	    }
+
+	    	}
+
+	    	if(!matched)
+	    	{
+	    	    newGroup = ++m_groupIDCounter;
+	    	    it->second.push_back(std::pair<F,uint32_t>(reversed_f, newGroup));
+	    	}
+	    	else
+	    	{
+	    	    newGroup = (it->second)[i].second;
+	    	}
+	    }
+	}
+	
+	return newGroup;
+    }
+    
+};
